48bd423849f3c6ff29ef700c6b31c87812039743
duc <hongducphannuyen@yahoo.com>
2016-05-30 12:47:10 +0700

    Khoi tao repo

---
+++ Model/DataBase.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Bề mặt để nói chuyện với những lớp khác
+ * Những lớp kết nối cơ sở dữ liệu phải implement lớp này
+ */
+interface IDataBase{
+    
+}
